2f030d1f62787e0fb9af8a3769637ae68212def0
devmarquinhos <marcos.emanuel.04@gmail.com>
2025-07-05 18:52:46 -0300

    :tada: initial commit

---
+++ src/main/java/com/devmarquinhos/octo/OctoApplication.java
@@ -0,0 +1,13 @@
+package com.devmarquinhos.octo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmarquinhos/octo/OctoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmarquinhos.octo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
